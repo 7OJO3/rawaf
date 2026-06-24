1ae1db1aa5da2d171e1381360a185b039b1c136f
KOOJOOOOO3 <jojoalrashidii@gmail.com>
2026-06-24 07:59:33 +0300

    Update index4.js

--- index4.js
+++ index4.js
@@ -123,8 +123,8 @@ client.on('messageCreate', async message => {
             new ButtonBuilder().setCustomId('map_roles').setLabel('شرح رتب').setStyle(ButtonStyle.Secondary),
             new ButtonBuilder().setCustomId('map_premium').setLabel('رتب مميزة').setStyle(ButtonStyle.Secondary),
             new ButtonBuilder().setCustomId('map_rooms').setLabel('شرح رومات').setStyle(ButtonStyle.Secondary)
-            .setThumbnail(CONFIG.IMG_7025.jpeg)
-            .setImage(CONFIG.IMG_5240.jpeg);
+            thumb: './IMG_7025.jpeg', // تأكدي من اسم الملف
+            mainImg: './IMG_5240.jpeg' // تأكدي من اسم الملف
         );
         await message.channel.send({ embeds: [new EmbedBuilder().setTitle("دليل سيرفر رواف").setColor(CONFIG.color)], components: [row] });
     }
